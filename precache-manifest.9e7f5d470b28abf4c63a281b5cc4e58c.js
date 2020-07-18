ead3ee57328bd04c077bfce734cc9dc341d70217
marcosTheCoder <marcosmenendez01@outlook.es>
2020-07-18 00:32:01 -0400

    Updates

---
+++ precache-manifest.9e7f5d470b28abf4c63a281b5cc4e58c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaa6fdb8cf2004a26fa6c54fa4f55719",
+    "url": "/Pat-and-Mat/index.html"
+  },
+  {
+    "revision": "19fe7c6d6853820183c6",
+    "url": "/Pat-and-Mat/static/css/2.7c048cb8.chunk.css"
+  },
+  {
+    "revision": "ff93dc941f51ee38731b",
+    "url": "/Pat-and-Mat/static/css/main.02b46c89.chunk.css"
+  },
+  {
+    "revision": "19fe7c6d6853820183c6",
+    "url": "/Pat-and-Mat/static/js/2.333c925e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pat-and-Mat/static/js/2.333c925e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff93dc941f51ee38731b",
+    "url": "/Pat-and-Mat/static/js/main.d8670134.chunk.js"
+  },
+  {
+    "revision": "61994172df7a7ce1016c",
+    "url": "/Pat-and-Mat/static/js/runtime-main.787ef65c.js"
+  },
+  {
+    "revision": "60265e4a1a36edfd6afab779bb46fd6f",
+    "url": "/Pat-and-Mat/static/media/bbanner.60265e4a.png"
+  },
+  {
+    "revision": "1220e4edf79dfdff59b4b9512aca29b1",
+    "url": "/Pat-and-Mat/static/media/copia.1220e4ed.png"
+  }
+]);
